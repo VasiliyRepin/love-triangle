0277d11c7eae7cf3fd0aa2273fca70060c3f3b3b
VasiliyRepin <36683162+VasiliyRepin@users.noreply.github.com>
2018-02-25 05:32:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,37 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    // your implementation
+  //  preferences.sort();
+    i = preferences.length;
+
+// document.write(preferences);
+     s = 0;
+    var n;
+    var z;
+    var k;
+    var x;
+    var y;
+    var sum;
+    var f = true;
+    for (k = 0; k<=i; k++) {
+
+
+        if (i<3)
+        {
+            f = false;
+        }
+
+    n = k+1;
+    z = preferences[k];
+    x = preferences[z-1];
+    y = preferences[x-1];
+
+        if (n===y && f==true)
+        {
+            s++;
+        }
+    }
+    sum = Math.floor(s/3);
+    return sum;
+}
